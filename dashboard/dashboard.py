ffe8d2d1d5195fc128ba4b4d49c38230a70391e2
Rahmi <120942852+dailyrahmi@users.noreply.github.com>
2024-10-07 12:27:39 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -170,20 +170,20 @@ if menu == "Analisis RFM":
 
     # Menghitung metrik RFM
     if all_data_day is not None:
-        all_data_day['date_day'] = pd.to_datetime(all_data_day['date_day'])  # Pastikan 'date_day' dalam format datetime
-        tanggal_referensi = all_data_day['date_day'].max()
-        all_data_day['Rentang_Hari'] = (tanggal_referensi - all_data_day['date_day']).dt.days
+        all_data_day['dteday'] = pd.to_datetime(all_data_day['dteday'])  # Pastikan 'dteday' dalam format datetime
+        tanggal_referensi = all_data_day['dteday'].max()
+        all_data_day['Rentang_Hari'] = (tanggal_referensi - all_data_day['dteday']).dt.days
 
         # Menghitung Frequency
-        frekuensi_penggunaan = all_data_day.groupby('date_day')['total_count'].sum().reset_index()
-        frekuensi_penggunaan.columns = ['date_day', 'Frekuensi']
+        frekuensi_penggunaan = all_data_day.groupby('dteday')['total_count'].sum().reset_index()
+        frekuensi_penggunaan.columns = ['dteday', 'Frekuensi']
 
         # Menghitung Monetary
-        moneter_pengguna = all_data_day.groupby('date_day')['registered'].sum().reset_index()
-        moneter_pengguna.columns = ['date_day', 'Nilai_Moneter']
+        moneter_pengguna = all_data_day.groupby('dteday')['registered'].sum().reset_index()
+        moneter_pengguna.columns = ['dteday', 'Nilai_Moneter']
 
         # Menggabungkan ketiga metrik RFM menjadi satu DataFrame
-        rfm_df = all_data_day[['date_day', 'Rentang_Hari']].merge(frekuensi_penggunaan, on='date_day', how='left').merge(moneter_pengguna, on='date_day', how='left')
+        rfm_df = all_data_day[['dteday', 'Rentang_Hari']].merge(frekuensi_penggunaan, on='dteday', how='left').merge(moneter_pengguna, on='dteday', how='left')
 
         # Visualisasi Hari Terbaik Berdasarkan Metrik RFM
         fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(30, 6))
@@ -192,21 +192,21 @@ if menu == "Analisis RFM":
         warna = ["#72BCD4"] * 5  # Warna yang digunakan untuk semua batang
 
         # Membuat plot untuk Recency
-        sns.barplot(y="Rentang_Hari", x="date_day", data=rfm_df.sort_values(by="Rentang_Hari").head(5), palette=warna, ax=ax[0])
+        sns.barplot(y="Rentang_Hari", x="dteday", data=rfm_df.sort_values(by="Rentang_Hari").head(5), palette=warna, ax=ax[0])
         ax[0].set_ylabel(None)
         ax[0].set_xlabel(None)
         ax[0].set_title("Recency (dalam hari)", loc="center", fontsize=18)
         ax[0].tick_params(axis='x', labelsize=15)
 
         # Membuat plot untuk Frequency
-        sns.barplot(y="Frekuensi", x="date_day", data=rfm_df.sort_values(by="Frekuensi", ascending=False).head(5), palette=warna, ax=ax[1])
+        sns.barplot(y="Frekuensi", x="dteday", data=rfm_df.sort_values(by="Frekuensi", ascending=False).head(5), palette=warna, ax=ax[1])
         ax[1].set_ylabel(None)
         ax[1].set_xlabel(None)
         ax[1].set_title("Frekuensi Peminjaman", loc="center", fontsize=18)
         ax[1].tick_params(axis='x', labelsize=15)
 
         # Membuat plot untuk Monetary
-        sns.barplot(y="Nilai_Moneter", x="date_day", data=rfm_df.sort_values(by="Nilai_Moneter", ascending=False).head(5), palette=warna, ax=ax[2])
+        sns.barplot(y="Nilai_Moneter", x="dteday", data=rfm_df.sort_values(by="Nilai_Moneter", ascending=False).head(5), palette=warna, ax=ax[2])
         ax[2].set_ylabel(None)
         ax[2].set_xlabel(None)
         ax[2].set_title("Nilai Moneter", loc="center", fontsize=18)
